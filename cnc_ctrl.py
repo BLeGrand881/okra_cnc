c63e87aef49079080d475d4c376c8be38a4f049b
Ben LeGrand <99503366+BLeGrand881@users.noreply.github.com>
2025-07-02 14:54:13 -0500

    control over individual axis instead of an entire cnc

--- cnc_ctrl.py
+++ cnc_ctrl.py
@@ -14,87 +14,44 @@ def extract_serial_number(symlink_path):
         
     print(f"Serial number not found for {symlink_path}")
 
-class cnc:
-    def __init__(self, x_symlink = None, y_symlink = None, z_symlink = None):
-
-        #create list of serial numbers
-        if not (x_symlink and y_symlink and z_symlink):
-            serial_nums = self.tic_x.list_connected_device_serial_numbers()
-        else:
-            serial_nums = [
-                extract_serial_number(x_symlink),
-                extract_serial_number(y_symlink), 
-                extract_serial_number(z_symlink)
-            ]
+class axis:
+    def __init__(self, symlink = None):
+        #create tic object
+        self.tic = pytic.Tic()
         
-        #create tic objects
-        self.tic_x = pytic.Tic()
-        self.tic_y = pytic.Tic()
-        self.tic_z = pytic.Tic()
+        #get serial number
+        serial_num = extract_serial_number(symlink)
 
-        #connect to tics
-        self.tic_x.connect_to_serial_number(serial_nums[0])
-        self.tic_y.connect_to_serial_number(serial_nums[1])
-        self.tic_z.connect_to_serial_number(serial_nums[2])
+        #connect to tic
+        self.tic.connect_to_serial_number(serial_num)
 
-        #load config file
-        self.tic_x.settings.load_config_file('/Users/banjo/Documents/Python/Okra/config.yml')
-        self.tic_y.settings.load_config_file('/Users/banjo/Documents/Python/Okra/config.yml')
-        self.tic_z.settings.load_config_file('/Users/banjo/Documents/Python/Okra/config.yml')
-
-        #apply settings
-        self.tic_x.settings.apply()
-        self.tic_y.settings.apply()
-        self.tic_z.settings.apply()
+        #load config file and apply settings
+        self.tic.settings.load_config_file('/Users/banjo/Documents/Python/Okra/config.yml')
+        self.tic.settings.apply()
 
         #stop + set position to 0
-        self.tic_x.halt_and_set_position(0)
-        self.tic_y.halt_and_set_position(0)
-        self.tic_z.halt_and_set_position(0)
+        self.tic.halt_and_set_position(0)
 
         #energize and exit safe start
-        self.tic_x.energize()
-        self.tic_y.energize()
-        self.tic_z.energize()
+        self.tic.energize()
+        self.tic.exit_safe_start()
 
-        self.tic_x.exit_safe_start()
-        self.tic_y.exit_safe_start()
-        self.tic_z.exit_safe_start()
-
-    def set_x_velocity(self, x_velocity):
-        self.tic_x.set_target_velocity(x_velocity)
-
-    def set_y_velocity(self, y_velocity):
-        self.tic_y.set_target_velocity(y_velocity)  
-    
-    def set_z_velocity(self, z_velocity):
-        self.tic_z.set_target_velocity(z_velocity)
+    def set_velocity(self, velocity):
+        self.tic.set_target_velocity(velocity)
 
 class pd:
-    def __init__(self, cnc, kp=0, kd=0, center = (0,0,0), has_z_axis = False):
+    def __init__(self, axis, kp=0, kd=0):
         self.kp = kp
         self.kd = kd
-        self.cnc = cnc
-        self.previous_errors = (0,0,0)
+        self.axis = axis
+        self.previous_error = 0
 
-        if not has_z_axis:
-            self.center = (center[0], center[1], 0)
-        else:
-            self.center = center
-
-    def update(self, measurements, dt=0.00033):
-        errors = (c - m for c,m in zip(self.center, measurements))
+    def update(self, error):
         
-        px, py, pz = (e*self.kp for e in errors)
+        p = error*self.kp
         
-        derivatives = ((e - self.previous_error) / dt for e in errors)
-        dx, dy, dz = (d*self.kd for d in derivatives)
+        d= (error-self.previous_error)*self.kd
 
-        self.cnc.set_x_velocity(px + dx)
-        self.cnc.set_y_velocity(py + dy)
-        self.cnc.set_z_velocity(pz + dz)
-        
-        self.previous_errors = errors
+        self.axis.set_velocity(p + d)
 
-
-        
+        self.previous_error = error        
